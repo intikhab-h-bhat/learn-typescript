56288b7daa1864919597ca3840e9f4405ea276c4
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2025-03-04 15:24:30 +0530

    variables

--- 01_Intro/variableMe.ts
+++ 01_Intro/variableMe.ts
@@ -4,7 +4,20 @@ let name : string = "John";
 let age : number;
 age=25;
 
+//we can declare it this way also as typscript is smart enough to understand the type of variable
+let num=25;
+
+
 console.log(name,age);
+
+
+// in these case it is best to use the type if we dpnt use type it will be any
+let hero : string;
+
+function getHero(){
+    return "Abc"
+}
+hero=getHero();
 
 
 export{}
