1dcc7e7684667285bc29bd26d5fff0e3de81e72f
Inrikhab <intikhabh@gmail.com>
2025-03-05 10:10:14 +0530

    inheritancs and Arrays

--- 01_Intro/4-type.ts
+++ 01_Intro/4-type.ts
@@ -17,3 +17,37 @@ let newuser:User= {_id:34,name:"Intikhab",age:22,email:"i@ih.com"}
 
 //newuser._id=45 //error
 console.log(newuser)
+
+
+type CardNumber={
+    cardnumber:number
+}
+type CardDate={
+    carddate:string
+}
+
+type CardDetails=CardNumber & CardDate
+
+
+//Arrays
+
+let superheros: string[]=[]
+let numarray:number[]=[]
+let difeerentarray:Array<string>=[]
+
+superheros.push("Ironman")
+numarray.push(1)
+difeerentarray.push("Ironman")
+
+
+
+type UserArray={
+    name:string,
+    age:number
+}
+
+let users:UserArray[]=[]
+
+users.push({name:"sai",age:22})
+users.push({name:"Intikhab",age:22})
+console.log(users)
